43f35e76d7c90bbd6f39cdd557e6192aae8e4734
pi-rate14 <apoorvasrivastava.14@gmail.com>
2022-12-02 21:30:49 +0530

    registered API-GATEWAY

--- currency-conversion-service/src/main/java/com/pirate14/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/pirate14/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
