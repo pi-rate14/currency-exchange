48514801d4ee56a687c04e711a8967bc7d2dda01
pi-rate14 <apoorvasrivastava.14@gmail.com>
2022-12-02 20:10:16 +0530

    registered services to erureka

--- currency-exchange-service/src/main/java/com/pirate14/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/pirate14/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,8 +1,8 @@
 package com.pirate14.microservices.currencyexchangeservice;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import java.math.BigDecimal;
 
